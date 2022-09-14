6579e40a318aecfbad596694168159a217eeddc3
shilpa vedre <shilpa.r.vedre@wellsfargo.com>
2022-09-14 20:23:09 +0000

    initial test

--- src/levelup/controller.py
+++ src/levelup/controller.py
@@ -16,7 +16,10 @@ class Direction(Enum):
 class GameStatus:
     running: bool = False
     character: Character = Character(DEFAULT_CHARACTER_NAME)
+    current_position: tuple = (-1,-1)
 
+    def set_character_position(self, xycoordinates: tuple) -> None:
+        print(f"Set character position state for testing")
 
 class GameController:
     status: GameStatus
